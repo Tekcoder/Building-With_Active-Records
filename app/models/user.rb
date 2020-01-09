0daacf10161234aa402e847da92fbcb30dfb07a2
Roheem Olayemi <abdurroheem20@gmail.com>
2020-01-09 18:38:57 +0100

    added the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
+  VALID_PASSWORD_REGEX = /\S*[`!@#$%^&*()]+\S*/
+	VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	
+	validates :name, presence: true,  uniqueness: true, length: {maximum: 250}
+	validates :password_digest, length: {minimum: 6}, format: {with: VALID_PASSWORD_REGEX}, presence: true
+  validates :email, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
+  
+  has_secure_password
+	has_many :posts
+	has_many :comments
 end
